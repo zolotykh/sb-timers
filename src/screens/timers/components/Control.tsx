f8a3233fd455a765ad6fd4f27cf9c8e49ff9f5b8
Alexander Zolotykh <alex.zolotyh@omega-r.com>
2020-11-25 20:02:18 +0300

    Add Control component

---
+++ src/screens/timers/components/Control.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useContext, useMemo, FC } from "react";
+import { Button } from "../../../components/button/Button";
+import { createPlanner } from "../../../utils/createPlanner";
+import { TWaitTime } from "../../../utils/createTimersQueue";
+import { ControlsContext } from "./ControlsContext";
+
+interface IControlProps {
+  /**
+   * Control button id
+   */
+  id: number;
+
+  time: TWaitTime;
+}
+
+export const Control: FC<IControlProps> = ({ id, time }) => {
+  const ctx = useContext(ControlsContext);
+  const onClick = useMemo(() => createPlanner({
+    id,
+    time,
+    log: ctx.log,
+    enqueue: ctx.addTimer,
+    // eslint-disable-next-line
+  }), [id, time]); // observe only props
+
+  return (
+    <li className={ctx.controlStyle}>
+      <Button text={`Кнопка ${id}`} onClick={onClick} />
+    </li>
+  );
+};
