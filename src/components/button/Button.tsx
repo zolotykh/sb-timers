ca40080067e28ce8c091f1c2b9ecfba799e52ca7
Alexander Zolotykh <alex.zolotyh@omega-r.com>
2020-11-26 20:24:44 +0300

    Improve code style

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { memo, FC } from "react";
 import styles from "./Button.module.css";
 
 interface IButtonProps {
@@ -7,10 +8,12 @@ interface IButtonProps {
   onClick?(): void;
 }
 
-export const Button = React.memo<IButtonProps>(({ text, onClick }) => {
+const ButtonComp: FC<IButtonProps> = ({ text, onClick }) => {
   return (
     <button type="button" className={styles.button} onClick={onClick}>
       {text}
     </button>
   );
-});
+};
+
+export const Button = memo(ButtonComp);
